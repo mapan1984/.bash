6c0ad4ad959450ebd2daf8bcec01a0ad81d86e02
mapan1984 <mapan1984@outlook.com>
2017-09-26 11:44:19 +0800

    add tmux config

--- bin/wsl.sh
+++ bin/wsl.sh
@@ -34,3 +34,15 @@ alias bg='cd /mnt/d/mapan1984/'
 # PATH=${PATH%%:}
 #echo $PATH
 
+### 默认开启tmux
+# [[ -z "$TMUX" && -n "$USE_TMUX" ]] && {
+#     [[ -n "$ATTACH_ONLY" ]] && {
+#         tmux a 2>/dev/null || {
+#             cd && exec tmux
+#         }
+#         exit
+#     }
+#
+#     tmux new-window -c "$PWD" 2>/dev/null && exec tmux a
+#     exec tmux
+# }
